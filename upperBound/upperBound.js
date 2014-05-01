c05f3765106f2ba25d186f40a3ac2778943df13a
Ali Nabavi <github@hourback.info>
2014-05-01 11:10:57 -0400

    - Implemented upperBound()

--- upperBound/upperBound.js
+++ upperBound/upperBound.js
@@ -14,6 +14,29 @@
 
 module.exports = function upperBound(guess) {
 
-    throw new Error('Not implemented yet :(');
+  var pattern = [2, 5, 10, 20, 50, 100, 200, 500, 1000];
+
+  var continuePattern = function (pattern) {
+    var nextFirstNumber = pattern[pattern.length-3];
+    pattern.push(nextFirstNumber * 10, nextFirstNumber * 25, nextFirstNumber * 50);
+    return pattern;
+  };
+
+  var getUpperBound = function (pattern, guess) {
+    var theUpperBound;
+
+    for (i=0; i < pattern.length; i++) {
+      if (pattern[i] > guess) {
+        theUpperBound = pattern[i];
+        break;
+      }
+      if (i === pattern.length - 1) {
+        pattern = continuePattern(pattern);
+      }
+    }
+    return theUpperBound;
+  };
+
+  return getUpperBound(pattern, guess);
     
 };
